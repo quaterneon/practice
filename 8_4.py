a2e70efa624df76104483b3d81db66ad479ac188
Agata Kargol <aukargol@gmail.com>
2017-08-17 08:56:35 -0700

    power set bit mask approach

--- 8_4.py
+++ 8_4.py
@@ -5,13 +5,13 @@
 test_cases = [{'a', 'b'}, {'a', 'b', 'c'}, {'a', 'b', 'c', 'd'}]
 
 
-def naive(case):
+def build_sets(case):
     """
-    O(n^2), kinda. For each item iter over sets we've built so far.
+    O(n*2^n). For each item iter over sets we've built so far.
     :param case:
     :return:
     """
-    sets = []
+    sets = [set()]
 
     for item in case:
         new_sets = []
@@ -21,11 +21,39 @@ def naive(case):
             new_set.add(item)
             new_sets.append(new_set)
 
-        new_sets.append(set(item))
         sets += new_sets
 
     return sets
 
 
 for case in test_cases:
-    print('{}: {}'.format(case, naive(case)))
+    print('{}: {}'.format(case, build_sets(case)))
+
+
+def bit_mask(case):
+    """
+    O(n*2^n). For each item iter over sets we've build so far.
+    :param case:
+    :return:
+    """
+    ordered = list(case)
+    fill = len(case)
+    num_sets = 2**(len(case))
+
+    sets = []
+
+    for mask in range(0, num_sets):
+        bit_arr = [int(char) for char in "{:b}".format(mask).zfill(fill)]
+        new_set = set()
+
+        for ind, val in enumerate(bit_arr):
+            if val:
+                new_set.add(ordered[ind])
+
+        sets.append(new_set)
+
+    return sets
+
+
+for case in test_cases:
+    print('{}: {}'.format(case, bit_mask(case)))
